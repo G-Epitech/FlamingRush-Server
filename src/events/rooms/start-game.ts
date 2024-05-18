7b7211e0893ce5fa21ac94b1636548eb83c271f1
Matheo Coquet <matheo.coquet@epitech.eu>
2024-05-18 15:42:46 +0200

    feat: add owner check for start game

--- src/events/rooms/start-game.ts
+++ src/events/rooms/start-game.ts
@@ -8,7 +8,13 @@ export const StartGame: IEvent<void> = {
   },
 
   handler(client, payload) {
-    for (const user of client.room?.users || []) {
+    const users = client.room?.users || [];
+    const user = users.find((user) => user.client.id === client.id);
+
+    if (!user?.owner)
+        return;
+
+    for (const user of users) {
       if (!user.ready && !user.owner) return;
     }
   },
