dbcdc20f025fd3529e73081971fbe3a2dbed0a1e
Flavien Chenu <flavienchenu@hotmail.com>
2024-05-18 13:28:39 +0200

    refactor(events): update client room

--- src/class/Client.ts
+++ src/class/Client.ts
@@ -15,8 +15,6 @@ export default class Client {
     }
 
     public disconnect() {
-        if (this.room) {
-            this.room.leave(this);
-        }
+        this.room?.leave(this);
     }
 }
