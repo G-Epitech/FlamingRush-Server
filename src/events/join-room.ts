9dc2e5f82ee9624af34502f2950d453d56d3f0cd
Flavien Chenu <flavienchenu@hotmail.com>
2024-05-18 13:47:13 +0200

    feat(events): add join Room

---
+++ src/events/join-room.ts
@@ -0,0 +1,20 @@
+import { IEvent } from "../interfaces/IEvent";
+import Room from "../class/Room";
+
+type JoinRoomPayload = {
+  profilePicture: number;
+  name: string;
+  code: string;
+}
+
+export const JoinRoom: IEvent<JoinRoomPayload> = {
+  name: "joinRoom",
+
+  handler(client, payload) {
+    const room: Room | undefined = client.server.rooms[payload.code];
+
+    if (!room)
+      return client.socket.emit("roomNotFound");
+    return room.register(client, payload.name, payload.profilePicture);
+  }
+};
